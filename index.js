f4c6c3906b01f16f03904b5be5a3ac97f62ac6d4
Ali Mahmoud <961alimahmoud@gmail.com>
2021-03-05 12:49:39 -0500

    second rule

--- index.js
+++ index.js
@@ -55,24 +55,47 @@
 
 // ============================================== //
 
-// What is the Big O of the below function? (Hint, you may want to go line by line)
-function anotherFunChallenge(input) {
-    let a = 5; // O(1)
-    let b = 10; // O(1)
-    let c = 50; // O(1)
-    for (let i = 0; i < input; i++) {
-        let x = i + 1; // O(n)
-        let y = i + 2; // O(n)
-        let z = i + 3; // O(n)
+// // What is the Big O of the below function? (Hint, you may want to go line by line)
+// function anotherFunChallenge(input) {
+//     let a = 5; // O(1)
+//     let b = 10; // O(1)
+//     let c = 50; // O(1)
+//     for (let i = 0; i < input; i++) {
+//         let x = i + 1; // O(n)
+//         let y = i + 2; // O(n)
+//         let z = i + 3; // O(n)
 
+//     }
+//     for (let j = 0; j < input; j++) {
+//         let p = j * 2; // O(n)
+//         let q = j * 2; // O(n)
+//     }
+//     let whoAmI = "I don't know"; // O(1)
+// }
+
+// // BIG O(4 + 5n)
+
+// // BIG O(n)
+
+// ============================================== //
+
+function printFirstItemThenFirstHalfThenSayHi100Times {
+    console.log(items[0]);
+
+    let middleIndex = Math.floor(items.length / 2)
+    let index = 0;
+
+    while (index < middleIndex) {
+        console.log(items[index]);
+        index++;
     }
-    for (let j = 0; j < input; j++) {
-        let p = j * 2; // O(n)
-        let q = j * 2; // O(n)
+
+    for (var i = 0; i < 100; i++) {
+        console.log(`hi`);
     }
-    let whoAmI = "I don't know"; // O(1)
 }
 
-// BIG O(4 + 5n)
-
-// BIG O (n)
+// O( 1 + n/2 + 100), 
+// O(n/2 + 101), # doesn't matter, becomes
+// O(n/2 + 1), worst case scenario, 1 doesn't matter
+// O(n)
