fff9cfa15bbcfa90b3e6e0fd14c54875a4d7a0a8
Ali Mahmoud <961alimahmoud@gmail.com>
2021-03-05 12:09:21 -0500

    constant time

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const nemo = ['nemo'];
+// const nemo = ['nemo'];
 
-const everyone = ['dory', 'bruce', 'marlin', `nemo`, `gill`, `bloat`, `nigel`, `squirt`, `darla`, `hank`]
+// const everyone = ['dory', 'bruce', 'marlin', `nemo`, `gill`, `bloat`, `nigel`, `squirt`, `darla`, `hank`]
 
-const large = new Array(10000).fill(`nemo`)
+// const large = new Array(10000).fill(`nemo`)
 
-function findNemo(array) {
-    let t0 = performance.now();
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === 'nemo') {
-            console.log('found nemo');
-        }
-    }
-    let t1 = performance.now();
-    console.log(`call to find Nemo took ${t1 - t0} milliseconds`);
+// function findNemo(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === 'nemo') {
+//             console.log('found nemo');
+//         }
+//     }
+// }
+
+// findNemo(large); // O(n) --> linear Time
+
+// ============================================== //
+
+const boxes = [0, 1, 2, 3, 4, 5]
+
+function logFirstTwiBoxes(boxes) {
+    console.log(boxes[0]);
+    console.log(boxes[1]);
 }
 
-findNemo(large);
+logFirstTwiBoxes(boxes)
