e93e56b56f323fac110a10e56ede1be28d73549e
Ali Mahmoud <961alimahmoud@gmail.com>
2021-03-05 12:17:48 -0500

    first exercise

--- index.js
+++ index.js
@@ -16,11 +16,39 @@
 
 // ============================================== //
 
-const boxes = [0, 1, 2, 3, 4, 5]
+// const boxes = [0, 1, 2, 3, 4, 5]
 
-function logFirstTwiBoxes(boxes) {
-    console.log(boxes[0]);
-    console.log(boxes[1]);
-}
+// function logFirstTwiBoxes(boxes) {
+//     console.log(boxes[0]); // O(1)
+//     console.log(boxes[1]); // O(1)
+// }
 
-logFirstTwiBoxes(boxes)
+// logFirstTwiBoxes(boxes) // O(2) 
+
+// constant time 
+
+// ============================================== //
+
+// function funChallenge(input) {
+//     let a = 10; //O(1)
+//     a = 50 + 3; // O(1)
+
+//     for (let i = 0; i < input.length; i++) { // O(n)/O(input)
+//         anotherFunction(); // O(n)
+//         let stranger = true; // O(n)
+//         a++; // O(n)
+//     }
+
+//     return a; // O(1)
+// }
+
+// //3 + 4n
+
+// // BIG O(3 + 4n);
+
+// // BIG O(n)
+
+
+// funChallenge();
+
+// ============================================== //
